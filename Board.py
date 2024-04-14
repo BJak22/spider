b6ed7208ab18c12d9d026ade2eae31e3493c41ce
BjakubowskiUKSW <b.jakubowski@student.uksw.edu.pl>
2024-04-14 20:08:22 +0200

    update 13.04

--- Board.py
+++ Board.py
@@ -34,8 +34,8 @@ class Board:
             val += 1
 
         # data of object that is moving
-        self.move_data = {"object": None, "x": 0, "y": 0, "startX": 0, "startY": 0, "card": None,
-                          "startPlace": None}
+        self.move_data = {"object": None, "x": 0, "y": 0, "startX": 0, "startY": 0, "cards": [],
+                          "startPlace": None, "card": None}
         self.add_cards("movable")
         self.move_bind("movable")
 
@@ -64,49 +64,115 @@ class Board:
         aux = (int(cardCoords[0]) - 150)
         aux = int(aux / 150)
         cardId = self.move_data["object"][-1]
+        self.move_data["startPlace"] = self.places[aux]
+        self.move_list = list()
         for i in self.cards:
             if cardId == i.id:
                 self.move_data["card"] = i
-        self.move_data["startPlace"] = self.places[aux]
-        self.move_list = list()
+        idList = list()
         if self.check_if_card_is_last():
             self.move_list.append(self.move_data["object"])
             self.canvas.tag_raise(self.move_data["object"])
+            idList.append(self.move_data["object"][0])
         else:
-            idList = self.canvas.find_overlapping(event.x, 60, event.x, 700)
+            idList.extend(self.canvas.find_overlapping(event.x, event.y, event.x, 700))
+            for i in idList:
+                for j in self.cards:
+                    if i == j.id and j.value> self.move_data["card"].value:
+                        idList.remove(i)
             for i in idList:
                 if i > 10:
                     self.move_list.append(i)
+        for i in self.cards:
+            for j in idList:
+                if j == i.id:
+                    self.move_data["cards"].append(i)
+
+    def partition(self,array, low, high):
+        pivot = array[high]
+
+        i = low - 1
+
+        for j in range(low, high):
+            if array[j].value <= pivot.value:
+                i = i + 1
+
+                (array[i], array[j]) = (array[j], array[i])
+
+        (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+        return i + 1
+
+    def quickSort(self,array, low, high):
+        if low < high:
+            pi = self.partition(array, low, high)
+
+            self.quickSort(array, low, pi - 1)
+
+            self.quickSort(array, pi + 1, high)
 
     def drop(self):
+        lenOfQueue = len(self.move_data["startPlace"].cards)
+        lenOfMoved = len(self.move_data["cards"])
+        leng = lenOfQueue - lenOfMoved
         cardCoords = self.canvas.coords(self.move_data["object"])
         # checking on which place the card is dropped
         aux = (int(cardCoords[0]) - 150)
         aux = int(aux / 150)
+        self.quickSort(self.move_data["cards"], 0, len(self.move_data["cards"]) -1)
+        self.move_data["cards"].reverse()
+        #print("przeniesiono:")
+        #for i in self.move_data["cards"]:
+            #print(i.value)
         if cardCoords[0] >= 150 and (self.places[aux].lastCard == None or
-                                     ((self.places[aux].lastCard.value - 1)== self.move_data["card"].value)):
+                                     ((self.places[aux].lastCard.value - 1) == self.move_data["cards"][-1].value)):
             self.places[aux].CoordY = 60 + (20 * (len(self.places[aux].cards)))
             x = self.places[aux].CoordX - cardCoords[0] + 50
             y = self.places[aux].CoordY - cardCoords[1] + 70
-            self.canvas.move(self.move_data["object"], x, y)
-            self.places[aux].cards.append(self.move_data["card"])
+            for i in self.move_list:
+                self.canvas.move(i, x, y)
+                y += 20
+            self.places[aux].cards.extend(self.move_data["cards"])
+            #print("karty na stosiku na ktory przeniosles:")
+            #for i in self.places[aux].cards:
+                #print(i.value)
             if self.places[aux].lastCard != None:
-                for i in self.cards:
-                    if (i.value == self.places[aux].lastCard.value and i.color == self.places[aux].lastCard.color and
-                            i.grid == self.places[aux].grid and
-                            self.places[aux].lastCard.color != self.move_data["card"].color):
-                        self.canvas.dtag(i.id, "movable")
-            self.places[aux].lastCard = self.move_data["card"]
-            self.move_data["card"].grid = self.places[aux].grid
-            self.move_data["startPlace"].cards.pop()
-            if len(self.move_data["startPlace"].cards) != 0:
+                i = 0
+                if self.places[aux].lastCard.color != self.move_data["card"].color:
+                    for i in self.cards:
+                        if i.grid == self.places[aux].grid:
+                            self.canvas.dtag(i.id, "movable")
+                self.places[aux].lastCard = self.move_data["cards"][-1]
+            if len(self.move_data["startPlace"].cards) > lenOfMoved:
+                for i in range(len(self.move_data["cards"])):
+                    self.move_data["startPlace"].cards.pop()
+                #print("karty ze stosika z ktotego przeniosles")
+                #for i in self.move_data["startPlace"].cards:
+                    #print(i.value)
                 self.move_data["startPlace"].lastCard = self.move_data["startPlace"].cards[-1]
-                for i in self.cards:
-                    if (i.value == self.move_data["startPlace"].lastCard.value
-                            and i.color == self.move_data["startPlace"].lastCard.color):
-                        self.canvas.addtag_withtag("movable", i.id)
+                listOfMovable = list()
+                i = len(self.move_data["startPlace"].cards) - 1
+                val = self.move_data["startPlace"].lastCard.value
+                while i >= 0 and self.move_data["startPlace"].cards[i].value == val \
+                    and self.move_data["startPlace"].cards[i].color == self.move_data["startPlace"].lastCard.color:
+                        listOfMovable.append(self.move_data["startPlace"].cards[i])
+                        i -= 1
+                        val += 1
+                print("movable ze starego")
+                for i in listOfMovable:
+                    print(i.value)
+                for i in listOfMovable:
+                    for j in self.cards:
+                        if (i.value == j.value and i.color == j.color) and j.grid == self.move_data["startPlace"].grid:
+                            self.canvas.addtag_withtag("movable", j.id)
+                        elif j.grid == self.move_data["startPlace"].grid:
+                            self.canvas.dtag(j.id, "movable")
+                            print(j.value)
             else:
+                #print("brak kart na stosiku z ktorego przeniosles")
                 self.move_data["startPlace"].lastCard = None
+                self.move_data["startPlace"].cards.clear()
+                self.move_data["startPlace"].CoordY = 60
         else:
             x = self.move_data["startX"] - cardCoords[0]
             y = self.move_data["startY"] - cardCoords[1]
@@ -117,6 +183,7 @@ class Board:
         if self.move_data["object"] != None:
             self.drop()
             self.move_data["card"] = None
+            self.move_data["cards"].clear()
             self.move_data["object"] = None
             self.move_data["x"] = 0
             self.move_data["y"] = 0
